5d1e7f075a77197dd9efe5c4d6aeedf0e24ab405
vish-dawange <vishalsd27@gmail.com>
2019-11-04 14:36:55 +0530

    A single-page web application for a set of cancer patient data. This project is called 'Patient Gateway'. The Patient Gateway should display a list of patients. Within the system patients are associated with several samples and each of these samples may have many variants

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # patient-gateway
+
+SPA to view the patient data, sample data and variants data.
+
+Dependency Requirements:
+-python3.5
+-flask
+
+Browser: 
+Mozzila firefox.
+
+In order to run the application, please clone the code from following link:
+https://github.com/vish-dawange/patient-gateway.git
+
+and run following command: python api.py
+or 
+python3 api.py #In case of multiple installations of python
+
+Use following URL to access the application:
+http://127.0.0.1:5000/
+
+Click on the 'View Patients' button to view all patents in the system.
+Click on any patient e.g. Emma L Vardy to view the samples associated.
+Click on any row to view variants data e.g. TUMOUR high quality (row 2)
+
+
---
+++ api.py
@@ -0,0 +1,52 @@
+from flask import Flask, g, request, render_template, jsonify, json
+import sqlite3
+
+
+app = Flask(__name__)
+
+# data initialization
+patientData=[]
+with open('patients.json') as f1:
+        patientData = json.load(f1)
+sampleData=[]
+with open('samples.json') as f2:
+        sampleData = json.load(f2)
+variantsData=[]
+with open('variants.json') as f3:
+        variantsData = json.load(f3)
+
+# Routes
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# get patients data end point
+@app.route('/getPatients', methods=['GET', 'POST'])
+def getPatients():
+    return jsonify(result=patientData)
+
+# get sample by patient id end point
+@app.route('/getSample', methods=['GET', 'POST'])
+def getSample():
+    patientId = request.args.get('patientId')
+    sampleList=[]
+    for sample in sampleData:
+        if sample['patientId']==int(patientId):
+            sampleList.append(sample)
+
+    return jsonify(result=sampleList)
+
+# get variants by sample id end point
+@app.route('/getVariants', methods=['GET', 'POST'])
+def getVariants():
+    sampleId = request.args.get('sampleId')
+    variantList=[]
+
+    for variant in variantsData:
+        if variant['sampleId']==int(sampleId):
+            variantList.append(variant)
+
+    return jsonify(result=variantList)
+
+if __name__ == '__main__':
+    app.run(debug=True)
